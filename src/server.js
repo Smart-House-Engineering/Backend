c8f55b8472104218ca16e317bfa7750c4a279f57
Liis U <liis.usin@gmail.com>
2024-02-19 22:12:05 +0100

    server setup for auth/routes and dependencies

--- src/server.js
+++ src/server.js
@@ -1,12 +1,35 @@
 import express from "express"
 const app = express()
-
-import { connectToDB } from "./configs/db.js"
-
 import dotenv from "dotenv"
 dotenv.config()
+import cookieParser from "cookie-parser"
+
+import { connectToDB } from "./configs/db.js"
+import { isAuthenticated } from "./middleware/authentication.js"
+import { isHomeUser, isExternalUser } from "./middleware/authorization.js"
+import authRoute from "./routes/auth.js"
+import homeUserRoute from "./routes/user.js"
+import externalUserRoute from "./routes/external.js"
+
 const port = process.env.PORT || 5000
 
+// Middleware
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+)
+app.use(express.json())
+app.use(cookieParser())
+
+// Routes
+app.use("/auth", authRoute)
+// Owner and tenant, primary users at home
+app.use("/api/homeUser", isAuthenticated, isHomeUser, homeUserRoute)
+// external users, such as nurses, with less privileges
+app.use("/api/externalUser", isAuthenticated, isExternalUser, externalUserRoute)
+
+// Database and Server connection
 connectToDB()
 
 app.listen(port, async () => {
