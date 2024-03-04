ebb7e5a0659b69bcf89ac29f36e246d650147a4e
ahmedmohd957 <ahmed.mohd957@gmail.com>
2024-03-04 19:15:40 +0100

    feat: :sparkles: Setup web-socket connection

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ import dotenv from "dotenv"
 dotenv.config()
 import cookieParser from "cookie-parser"
 import cors from "cors"
+import { createServer } from "http"
+import { Server } from "socket.io"
 
 import { connectToDB } from "./configs/db.js"
 import { isAuthenticated } from "./middleware/authentication.js"
@@ -15,6 +17,15 @@ import smartHomeRoutes from "./routes/smartHome.js"
 import modes from "./routes/modes.js"
 
 const port = process.env.PORT || 5000
+
+// Websocket initialization
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+})
 
 // Middleware
 app.use(cors({ origin: true, credentials: true }))
@@ -40,4 +51,26 @@ connectToDB()
 
 app.listen(port, async () => {
   console.log("Server listening on PORT " + port)
+})
+
+// WebSocket connection
+io.on("connection", socket => {
+  console.log("A client connected")
+
+  // Emit a message back to the client upon connection
+  socket.emit("message", "Hello from server!")
+
+  // Handle events from the client
+  socket.on("connect", data => {
+    console.log("Received data from client:", data)
+  })
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("Client disconnected")
+  })
+})
+
+httpServer.listen(4000, () => {
+  console.log("Socket listening on PORT " + 4000)
 })
