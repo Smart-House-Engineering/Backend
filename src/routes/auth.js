91b035a5c23b0ecf08f86d503fb9f4ac9595183f
Liis U <liis.usin@gmail.com>
2024-03-04 21:36:11 +0100

    fix 2

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -132,8 +132,8 @@ route.post("/login", async function (req, res) {
 
     res.cookie("SmartHouseToken", token, {
       httpOnly: true,
-      sameSite: "None",
       secure: true,
+      sameSite: "None",
     })
 
     return res.status(200).json({ message: "Login successful" })
